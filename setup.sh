9c2491c6588b209c238e62742db0028f255a326b
Filip Sondej <filipsondej@protonmail.com>
2021-08-16 21:38:39 +0200

    cleanup

--- setup.sh
+++
@@ -1,7 +0,0 @@
-# install pipenv
-# install env from Pipfile
-# install jupyter?
-# jupyter nbextension enable --py --sys-prefix ipyevents
-# jupyter labextension install @jupyter-widgets/jupyterlab-manager ipyevents
-
-# maybe migrating interface into stremlit would be better?
